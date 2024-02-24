f3f44588a8fdad1d376cb36560f37baf95d061ed
Sandhavi Wanigasooriya <sandhaviwanigasooriya@gmail.com>
2024-02-24 09:49:14 +0530

    updated login function

--- Include/connectin.php
+++ Include/connectin.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
+
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>connect-php</title>
 </head>
 
 <body>
-    <?php 
+    <?php
     $dbhost = 'localhost';
     $dbuser = 'root';
     $dbpass = '';
@@ -17,13 +18,12 @@
     // echo 'connected successfully';
 
     // echo "<hr>";
-    $db = mysqli_select_db($conn, 'party'); 
+    $db = mysqli_select_db($conn, 'party');
 
     if (!$db) {
         echo 'Error Check Name of the Database';
     } else {
         echo 'Database Sucessfully Connected';
-       
     }
     ?>
 </body>
