ddac69b1c647a65aa87792791b572ffba1342dcb
Sandhavi Wanigasooriya <sandhaviwanigasooriya@gmail.com>
2024-02-27 14:09:16 +0530

    Add viewport meta element and alt attributes to images

--- Pages/Dashboard 2/admmindashboard.php
+++ Pages/Dashboard 2/admmindashboard.php
@@ -2,6 +2,8 @@
 <html>
   <head>
     <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    // Add viewport meta element
     <link rel="stylesheet" href="globals.css" />
     <link rel="stylesheet" href="styleguide.css" />
     <link rel="stylesheet" href="style.css" />
@@ -32,17 +34,23 @@
                 <span class="span">data</span>
               </p>
               <div class="page">
-                <img class="dropdown" src="img/dropdown.svg" />
+                <img class="dropdown" src="img/dropdown.svg" alt="new" />
                 <div class="element">
-                  <div class="overlap-group"><div class="text-wrapper-11">1</div></div>
+                  <div class="overlap-group">
+                    <div class="text-wrapper-11">1</div>
+                  </div>
                 </div>
                 <div class="overlap-wrapper">
-                  <div class="overlap"><div class="text-wrapper-12">2</div></div>
+                  <div class="overlap">
+                    <div class="text-wrapper-12">2</div>
+                  </div>
                 </div>
                 <div class="overlap-wrapper">
-                  <div class="overlap"><div class="text-wrapper-12">3</div></div>
+                  <div class="overlap">
+                    <div class="text-wrapper-12">3</div>
+                  </div>
                 </div>
-                <img class="dropdown" src="img/dropdown-2.svg" />
+                <img class="dropdown" src="img/dropdown-2.svg" alt="new" />
               </div>
             </div>
             <div class="overlap-2">
@@ -61,30 +69,36 @@
                       <div class="div-wrapper">
                         <div class="overlap-group-2">
                           <div class="placeholder"></div>
-                          <img class="img" src="img/call.svg" />
+                          <img class="img" src="img/call.svg" alt="new" />
                         </div>
                       </div>
                       <div class="div-wrapper">
                         <div class="overlap-group-2">
                           <div class="placeholder"></div>
-                          <img class="img" src="img/email.svg" />
+                          <img class="img" src="img/email.svg" alt="new" />
                         </div>
                       </div>
                     </div>
                     <div class="selected"></div>
-                    <div class="checkedbox"><img class="vector" src="img/vector-8.svg" /></div>
+                    <div class="checkedbox">
+                      <img class="vector" src="img/vector-8.svg" alt="new" />
+                    </div>
                     <div class="customer">
                       <div class="placeholder-2"></div>
-                      <div class="name"><div class="text-wrapper-13">Samanta William</div></div>
+                      <div class="name">
+                        <div class="text-wrapper-13">Samanta William</div>
+                      </div>
                     </div>
                     <div class="text-wrapper-14">March 25, 2021</div>
                     <div class="text-wrapper-15">Mana William</div>
                     <div class="text-wrapper-16">Jakarta</div>
                     <div class="text-wrapper-17">#123456789</div>
                     <div class="status">
-                      <div class="content"><div class="text-wrapper-18">Basic</div></div>
+                      <div class="content">
+                        <div class="text-wrapper-18">Basic</div>
+                      </div>
                     </div>
-                    <img class="dots" src="img/dots.svg" />
+                    <img class="dots" src="img/dots.svg" alt="new" />
                   </div>
                 </div>
                 <div class="overlap-group-wrapper">
@@ -93,29 +107,33 @@
                       <div class="div-wrapper">
                         <div class="overlap-group-2">
                           <div class="placeholder"></div>
-                          <img class="img" src="img/call.svg" />
+                          <img class="img" src="img/call.svg" alt="new" />
                         </div>
                       </div>
                       <div class="div-wrapper">
                         <div class="overlap-group-2">
                           <div class="placeholder"></div>
-                          <img class="img" src="img/email.svg" />
+                          <img class="img" src="img/email.svg" alt="new" />
                         </div>
                       </div>
                     </div>
                     <div class="text-wrapper-16">Jakarta</div>
                     <div class="content-wrapper">
-                      <div class="content-2"><div class="text-wrapper-18">Standard</div></div>
+                      <div class="content-2">
+                        <div class="text-wrapper-18">Standard</div>
+                      </div>
                     </div>
                     <div class="text-wrapper-15">James Soap</div>
                     <div class="check-2"></div>
                     <div class="customer">
                       <div class="placeholder-2"></div>
-                      <div class="name"><div class="text-wrapper-13">Tony Soap</div></div>
+                      <div class="name">
+                        <div class="text-wrapper-13">Tony Soap</div>
+                      </div>
                     </div>
                     <div class="text-wrapper-14">March 25, 2021</div>
                     <div class="text-wrapper-17">#123456789</div>
-                    <img class="dots" src="img/dots.svg" />
+                    <img class="dots" src="img/dots.svg" alt="new" />
                   </div>
                 </div>
                 <div class="overlap-group-wrapper">
@@ -124,30 +142,36 @@
                       <div class="div-wrapper">
                         <div class="overlap-group-2">
                           <div class="placeholder"></div>
-                          <img class="img" src="img/call.svg" />
+                          <img class="img" src="img/call.svg" alt="new" />
                         </div>
                       </div>
                       <div class="div-wrapper">
                         <div class="overlap-group-2">
                           <div class="placeholder"></div>
-                          <img class="img" src="img/email.svg" />
+                          <img class="img" src="img/email.svg" alt="new" />
                         </div>
                       </div>
                     </div>
                     <div class="selected"></div>
-                    <div class="checkedbox"><img class="vector" src="img/vector-8.svg" /></div>
+                    <div class="checkedbox">
+                      <img class="vector" src="img/vector-8.svg" alt="new" />
+                    </div>
                     <div class="customer">
                       <div class="placeholder-2"></div>
-                      <div class="name"><div class="text-wrapper-13">Karen Hope</div></div>
+                      <div class="name">
+                        <div class="text-wrapper-13">Karen Hope</div>
+                      </div>
                     </div>
                     <div class="text-wrapper-14">March 25, 2021</div>
                     <div class="text-wrapper-15">Justin Hope</div>
                     <div class="text-wrapper-16">Jakarta</div>
                     <div class="text-wrapper-17">#123456789</div>
                     <div class="status-2">
-                      <div class="content"><div class="text-wrapper-18">Premium</div></div>
+                      <div class="content">
+                        <div class="text-wrapper-18">Premium</div>
+                      </div>
                     </div>
-                    <img class="dots" src="img/dots.svg" />
+                    <img class="dots" src="img/dots.svg" alt="new" />
                   </div>
                 </div>
                 <div class="overlap-group-wrapper">
@@ -156,29 +180,33 @@
                       <div class="div-wrapper">
                         <div class="overlap-group-2">
                           <div class="placeholder"></div>
-                          <img class="img" src="img/call.svg" />
+                          <img class="img" src="img/call.svg" alt="new" />
                         </div>
                       </div>
                       <div class="div-wrapper">
                         <div class="overlap-group-2">
                           <div class="placeholder"></div>
-                          <img class="img" src="img/email.svg" />
+                          <img class="img" src="img/email.svg" alt="new" />
                         </div>
                       </div>
                     </div>
                     <div class="text-wrapper-16">Jakarta</div>
                     <div class="content-wrapper">
-                      <div class="content-2"><div class="text-wrapper-18">Standard</div></div>
+                      <div class="content-2">
+                        <div class="text-wrapper-18">Standard</div>
+                      </div>
                     </div>
                     <div class="text-wrapper-15">Amanda Nico</div>
                     <div class="check-2"></div>
                     <div class="customer">
                       <div class="placeholder-2"></div>
-                      <div class="name"><div class="text-wrapper-13">Jordan Nico</div></div>
+                      <div class="name">
+                        <div class="text-wrapper-13">Jordan Nico</div>
+                      </div>
                     </div>
                     <div class="text-wrapper-14">March 25, 2021</div>
                     <div class="text-wrapper-17">#123456789</div>
-                    <img class="dots" src="img/dots.svg" />
+                    <img class="dots" src="img/dots.svg" alt="new" />
                   </div>
                 </div>
                 <div class="overlap-group-wrapper">
@@ -187,30 +215,36 @@
                       <div class="div-wrapper">
                         <div class="overlap-group-2">
                           <div class="placeholder"></div>
-                          <img class="img" src="img/call.svg" />
+                          <img class="img" src="img/call.svg" alt="new" />
                         </div>
                       </div>
                       <div class="div-wrapper">
                         <div class="overlap-group-2">
                           <div class="placeholder"></div>
-                          <img class="img" src="img/email.svg" />
+                          <img class="img" src="img/email.svg" alt="new" />
                         </div>
                       </div>
                     </div>
                     <div class="selected"></div>
-                    <div class="checkedbox"><img class="vector" src="img/vector-8.svg" /></div>
+                    <div class="checkedbox">
+                      <img class="vector" src="img/vector-8.svg" alt="new" />
+                    </div>
                     <div class="customer">
                       <div class="placeholder-2"></div>
-                      <div class="name"><div class="text-wrapper-13">Nadila Adja</div></div>
+                      <div class="name">
+                        <div class="text-wrapper-13">Nadila Adja</div>
+                      </div>
                     </div>
                     <div class="text-wrapper-14">March 25, 2021</div>
                     <div class="text-wrapper-15">Jack Adja</div>
                     <div class="text-wrapper-16">Jakarta</div>
                     <div class="text-wrapper-17">#123456789</div>
                     <div class="status">
-                      <div class="content"><div class="text-wrapper-18">Basic</div></div>
+                      <div class="content">
+                        <div class="text-wrapper-18">Basic</div>
+                      </div>
                     </div>
-                    <img class="dots" src="img/dots.svg" />
+                    <img class="dots" src="img/dots.svg" alt="new" />
                   </div>
                 </div>
                 <div class="overlap-group-wrapper">
@@ -219,29 +253,33 @@
                       <div class="div-wrapper">
                         <div class="overlap-group-2">
                           <div class="placeholder"></div>
-                          <img class="img" src="img/call.svg" />
+                          <img class="img" src="img/call.svg" alt="new" />
                         </div>
                       </div>
                       <div class="div-wrapper">
                         <div class="overlap-group-2">
                           <div class="placeholder"></div>
-                          <img class="img" src="img/email.svg" />
+                          <img class="img" src="img/email.svg" alt="new" />
                         </div>
                       </div>
                     </div>
                     <div class="text-wrapper-16">Jakarta</div>
                     <div class="content-wrapper">
-                      <div class="content-2"><div class="text-wrapper-18">Standard</div></div>
+                      <div class="content-2">
+                        <div class="text-wrapper-18">Standard</div>
+                      </div>
                     </div>
                     <div class="text-wrapper-15">Danny Ahmad</div>
                     <div class="check-2"></div>
                     <div class="customer">
                       <div class="placeholder-2"></div>
-                      <div class="name"><div class="text-wrapper-13">Johnny Ahmad</div></div>
+                      <div class="name">
+                        <div class="text-wrapper-13">Johnny Ahmad</div>
+                      </div>
                     </div>
                     <div class="text-wrapper-14">March 25, 2021</div>
                     <div class="text-wrapper-17">#123456789</div>
-                    <img class="dots" src="img/dots.svg" />
+                    <img class="dots" src="img/dots.svg" alt="new" />
                   </div>
                 </div>
               </div>
@@ -251,19 +289,19 @@
         <div class="student-menu">
           <div class="new-student-button">
             <div class="content-3">
-              <img class="img-2" src="img/image.svg" />
+              <img class="img-2" src="img/image.svg" alt="new" />
               <div class="text-wrapper-19">New Student</div>
             </div>
           </div>
           <div class="sort">
             <div class="content-4">
               <div class="text-wrapper-20">Newest</div>
-              <img class="dropdown" src="img/dropdown-1.svg" />
+              <img class="dropdown" src="img/dropdown-1.svg" alt="new" />
             </div>
           </div>
           <div class="search-bar">
             <div class="frame">
-              <img class="search" src="img/search.svg" />
+              <img class="search" src="img/search.svg" alt="new" />
               <div class="text-wrapper-21">Search here...</div>
             </div>
           </div>
@@ -276,11 +314,13 @@
           </div>
           <div class="notification">
             <div class="overlap-group-3">
-              <img class="bell" src="img/bell.svg" />
+              <img class="bell" src="img/bell.svg" alt="new" />
               <div class="ellipse"></div>
             </div>
           </div>
-          <div class="setting"><img class="gear" src="img/gear.svg" /></div>
+          <div class="setting">
+            <img class="gear" src="img/gear.svg" alt="new" />
+          </div>
         </div>
       </div>
     </div>
