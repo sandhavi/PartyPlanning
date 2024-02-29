9c66873a55a8bc20ebe1f488ee1d166d38f696f3
Sandhavi Wanigasooriya <sandhaviwanigasooriya@gmail.com>
2024-02-29 13:24:46 +0530

    Refactor user save functionality

--- Backend/usersave.php
+++ Backend/usersave.php
@@ -1,23 +1,18 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Include the database connection file
     include '../Include/connectin.php';
 
-    // Get user input from the form
     $email = $_POST['email'];
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Prepare and execute the SQL query
     $stmt = $conn->prepare("INSERT INTO customer (email, username, password) VALUES (?, ?, ?)");
     $stmt->bind_param("sss", $email, $username, $password);
     $stmt->execute();
 
-    // Close the statement and database connection
     $stmt->close();
     $conn->close();
 
-    // Redirect to the login page
     echo "<script>alert('Login successful'); 
             window.location.href = '../Pages/Login/LoginUser.html';</script>";
     exit();
