cb4f97bff835337b01cb5f3e9559d9b6d58869a4
Sandhavi Wanigasooriya <sandhaviwanigasooriya@gmail.com>
2024-03-05 21:33:56 +0530

    Delete unused image and script files

--- Pages/DummmyPage/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('nextButton').addEventListener('click', function() {
-    alert('Next button clicked!');
-});
-
-// Add more JavaScript to handle other interactions and animations
